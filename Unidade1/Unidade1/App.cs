883aff5522e27f56a7a6193995dd77790095b160
Pedro <paf.victor@gmail.com>
2022-11-09 23:51:19 -0300

    Conclusão 2 exercício.

--- Unidade1/Unidade1/App.cs
+++ Unidade1/Unidade1/App.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
+using System.Xml.Linq;
 using Unidade1.ProblemaVertice;
 using Unidade1.PyramidProblem;
 
@@ -10,6 +11,20 @@ using Unidade1.PyramidProblem;
 //PyramidProblem(4);
 class App
 {
+
+
+    static void getXY(ref Vertice v1, ref Vertice v2)
+    {
+        Console.WriteLine("Digite, respectivamente X e Y do vertice 1");
+        float X = (float)Convert.ToDouble(Console.ReadLine());
+        float Y = (float)Convert.ToDouble(Console.ReadLine());
+        v1 = new Vertice(X, Y);
+        Console.WriteLine("Digite, respectivamente X e Y do vertice 2");
+        X = (float)Convert.ToDouble(Console.ReadLine());
+        Y = (float)Convert.ToDouble(Console.ReadLine());
+        v2 = new Vertice(X, Y);
+    }
+
     static void Main(string[] args)
     {
         int option_user = 1;
@@ -26,17 +41,76 @@ class App
                     Console.WriteLine("Digite a quantidade de linhas da piramide");
                     int quantLinhas = Convert.ToInt32(Console.ReadLine());
                     Pyramid.PyramidProblem(quantLinhas);
-                break;
+                    break;
                 
                 case 2:
-                    Console.WriteLine("Escolheu Vertice");
-                break;
+                    Vertice v1 = null;
+                    Vertice v2 = null;
+                    Console.WriteLine("1 - Calcular distância entre 2 vertices");
+                    Console.WriteLine("2 - Mover um vertice para um ponto (X,Y)");
+                    Console.WriteLine("3 - Verificar se V1 e V2 são iguais");
+                    int escolhaVertice = Convert.ToInt32(Console.ReadLine());
+                    switch (escolhaVertice)
+                    {
+                        case 1:
+                            getXY(ref v1, ref v2);
+                            Console.WriteLine("V1 -> " + v1.toString());
+                            Console.WriteLine("V2 -> " + v2.toString());
+                            Console.WriteLine("Distancia entre V1 e V2 = " + v1.euclidianDistance(v2));
+                            break;
+
+                        case 2:
+                            if (v1 == null && v2 == null)
+                            {
+                                getXY(ref v1, ref v2);                            
+                            }
+                            Console.WriteLine("1 - Mover Vertice 1 (" + v1.toString() + ")");
+                            Console.WriteLine("2 - Mover Vertice 2 (" + v2.toString() + ")");
+                            int movVertice = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Digite novos valores para X e Y");
+                            float X = (float)Convert.ToDouble(Console.ReadLine());
+                            float Y = (float)Convert.ToDouble(Console.ReadLine());
+                            switch (movVertice)
+                            {
+                                case 1:
+                                    Console.WriteLine("Antes de mover => " + v1.toString());
+                                    v1.moveVertice(X, Y);
+                                    Console.WriteLine("Depois de mover => " + v1.toString());
+                                    break;
+                                case 2:
+                                    Console.WriteLine("Antes de mover => " + v1.toString());
+                                    v2.moveVertice(X, Y);
+                                    Console.WriteLine("Depois de mover => " + v1.toString());
+                                    break;
+                            }                                                       
+                        break;
+
+                        case 3:
+                            if (v1 == null && v2 == null)
+                            {
+                                getXY(ref v1, ref v2);
+                            }
+                            Console.WriteLine("V1 => "+ v1.toString());
+                            Console.WriteLine("V2 => "+ v2.toString());
+                            Console.WriteLine("São iguais? R = " + v1.equalVertice(v2));
+                            break;
+
+                        default:
+                            
+                            break;
+                    }
+                    
+
+                    break;
+
+
+                default:
+                    break;
             }
         }
 
 
-        Vertice v1 = new Vertice((float)2.1, (float)3.2);
-        Console.Write(v1.toString());
+ 
     }
 }
 
--- Unidade1/Unidade1/ProblemaVertice/Vertice.cs
+++ Unidade1/Unidade1/ProblemaVertice/Vertice.cs
@@ -26,6 +26,25 @@ namespace Unidade1.ProblemaVertice
             return this.Y;
         }
 
+        public void moveVertice(float X, float Y)
+        {
+            this.X = X; this.Y = Y;
+        }
+
+        public bool equalVertice(Vertice v2)
+        {
+            if (this.X == v2.getX() && this.Y == v2.getY())
+            {
+                return true;
+            }
+            return false;
+        }
+        public float euclidianDistance(Vertice v2)
+        {
+            double somaPotencia = Math.Pow(v2.getX() - this.getX(), 2) + Math.Pow(v2.getY() - this.getY(), 2);
+            float d = (float)Math.Sqrt(somaPotencia);
+            return d;
+        }
         
         public String toString()
         {
