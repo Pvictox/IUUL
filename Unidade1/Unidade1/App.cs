796c4e48f6eb1f6907572f12e8cdf808f9e420ff
Pedro <paf.victor@gmail.com>
2022-11-10 09:03:09 -0300

    Exercicio 3 Feito

--- Unidade1/Unidade1/App.cs
+++ Unidade1/Unidade1/App.cs
@@ -2,6 +2,7 @@
 
 
 using System.Xml.Linq;
+using Unidade1.Formas;
 using Unidade1.ProblemaVertice;
 using Unidade1.PyramidProblem;
 
@@ -13,26 +14,24 @@ class App
 {
 
 
-    static void getXY(ref Vertice v1, ref Vertice v2)
+    static void getXY(ref Vertice v1, int count)
     {
-        Console.WriteLine("Digite, respectivamente X e Y do vertice 1");
+        Console.WriteLine("Digite, respectivamente X e Y do vertice "+count );
         float X = (float)Convert.ToDouble(Console.ReadLine());
         float Y = (float)Convert.ToDouble(Console.ReadLine());
         v1 = new Vertice(X, Y);
-        Console.WriteLine("Digite, respectivamente X e Y do vertice 2");
-        X = (float)Convert.ToDouble(Console.ReadLine());
-        Y = (float)Convert.ToDouble(Console.ReadLine());
-        v2 = new Vertice(X, Y);
     }
 
     static void Main(string[] args)
     {
         int option_user = 1;
-        while (option_user != 3)
+        float X, Y;
+        while (option_user != 4)
         {
             Console.WriteLine("=================");
             Console.WriteLine("1 - Problema Piramide");
             Console.WriteLine("2 - Problema Vertice");
+            Console.WriteLine("3 - Problema Triangulo");
             Console.Write("Digite o valor: ");
             option_user = Convert.ToInt32(Console.ReadLine());
             switch (option_user)
@@ -53,23 +52,25 @@ class App
                     switch (escolhaVertice)
                     {
                         case 1:
-                            getXY(ref v1, ref v2);
+                            getXY(ref v1, 1);
+                            getXY(ref v2, 2);
                             Console.WriteLine("V1 -> " + v1.toString());
                             Console.WriteLine("V2 -> " + v2.toString());
                             Console.WriteLine("Distancia entre V1 e V2 = " + v1.euclidianDistance(v2));
                             break;
 
                         case 2:
-                            if (v1 == null && v2 == null)
+                            if (v1 == null || v2 == null)
                             {
-                                getXY(ref v1, ref v2);                            
+                                getXY(ref v1, 1);
+                                getXY(ref v2, 2);
                             }
                             Console.WriteLine("1 - Mover Vertice 1 (" + v1.toString() + ")");
                             Console.WriteLine("2 - Mover Vertice 2 (" + v2.toString() + ")");
                             int movVertice = Convert.ToInt32(Console.ReadLine());
                             Console.WriteLine("Digite novos valores para X e Y");
-                            float X = (float)Convert.ToDouble(Console.ReadLine());
-                            float Y = (float)Convert.ToDouble(Console.ReadLine());
+                            X = (float)Convert.ToDouble(Console.ReadLine());
+                            Y = (float)Convert.ToDouble(Console.ReadLine());
                             switch (movVertice)
                             {
                                 case 1:
@@ -86,9 +87,10 @@ class App
                         break;
 
                         case 3:
-                            if (v1 == null && v2 == null)
+                            if (v1 == null || v2 == null)
                             {
-                                getXY(ref v1, ref v2);
+                                getXY(ref v1, 1);
+                                getXY(ref v2, 2);
                             }
                             Console.WriteLine("V1 => "+ v1.toString());
                             Console.WriteLine("V2 => "+ v2.toString());
@@ -103,7 +105,45 @@ class App
 
                     break;
 
+                case 3:
+                    v1 = null;
+                    v2 = null;
+                    Vertice v3 = null;
+                    getXY(ref v1, 1);
+                    getXY(ref v2, 2);
+                    getXY(ref v3, 3);
+                    Triangulo t1 = new Triangulo(v1, v2, v3);
+                    
+                    if (t1.GetVertices().Count != 0)
+                    {
+                        Console.WriteLine("1 - Comparar com outro Triângulo");
+                        Console.WriteLine("2 - Verificar perimetro");
+                        int escolhaTriangulo = Convert.ToInt32(Console.ReadLine());
+                        switch (escolhaTriangulo)
+                        {
+                            case 1:
+                                v1 = null;
+                                v2 = null;
+                                v3 = null;
+                                getXY(ref v1, 1);
+                                getXY(ref v2, 2);
+                                getXY(ref v3, 3);
+                                Triangulo t2 = new Triangulo(v1, v2, v3);
+                                if (t2.GetVertices().Count != 0)
+                                {
+                                    Console.WriteLine("São semelhantes? R = " + t1.isSemelhanteTriangulo(t2));
+                                    Console.WriteLine("São iguais? R = " + t1.isEqualTriangulo(t2));
+                                }
+                                break;
 
+                            case 2:
+                                Console.WriteLine(t1.toString()) ;
+                                Console.WriteLine("Perimetro do triangulo = " + t1.getPerimetro());
+                                break;
+                        }
+                    }
+
+                    break;
                 default:
                     break;
             }
---
+++ Unidade1/Unidade1/Formas/Triangulo.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unidade1.ProblemaVertice;
+
+namespace Unidade1.Formas
+{
+    public class Triangulo
+    {
+        private enum tipoTriangulo
+        {
+            EQUILATERO = 0,
+            ISOSCEÇES = 1,
+            ESCALENO = 2
+        }
+
+        private List<Vertice> vertices = new List<Vertice>();
+        private List<float> medidasLados = new List<float>();
+        private float perimetro = 0;
+        private int idTipoTriangulo = 0;
+        private float AreaTriangulo = 0;
+
+        public Triangulo(Vertice v1, Vertice v2, Vertice v3)
+        {
+            if (checkIfTriangule(v1, v2, v3))
+            {
+                this.GetVertices().Add(v1);
+                this.GetVertices().Add(v2);
+                this.GetVertices().Add(v3);
+                this.calculaPerimetro();
+                this.assignTipoTriangulo();
+                this.calcularAreaTriangulo();
+
+            }
+            else {
+                Console.Write("Não é possível existir esse triangulo");
+            }
+        }
+
+
+        public void calcularAreaTriangulo()
+        {
+            float lado1 = this.getMedidasLados().ElementAt(0);
+            float lado2 = this.getMedidasLados().ElementAt(1);
+            float lado3 = this.getMedidasLados().ElementAt(2);
+            float S = this.getPerimetro() / 2;
+            this.AreaTriangulo = (float)Math.Sqrt( S * (S-lado1) * (S-lado2) * (S-lado3));
+
+        }
+
+        public String getTipoTriangulo()
+        {
+            var enumDisplay = (tipoTriangulo)this.idTipoTriangulo;
+            return enumDisplay.ToString();
+        }
+        public void assignTipoTriangulo()
+        {
+            float lado1 = this.getMedidasLados().ElementAt(0);
+            float lado2 = this.getMedidasLados().ElementAt(1);
+            float lado3 = this.getMedidasLados().ElementAt(2);
+
+            if ((lado1 == lado2) && (lado2 == lado3))
+            {
+                this.idTipoTriangulo = (int)tipoTriangulo.EQUILATERO;
+            }else if ( (lado2 != lado3) && (lado1 != lado2) && (lado1 != lado3))
+            {
+                this.idTipoTriangulo = (int)tipoTriangulo.ESCALENO;
+            }
+            else
+            {
+                this.idTipoTriangulo = (int)tipoTriangulo.ISOSCEÇES;
+            }
+        }
+        public List<float> getMedidasLados()
+        {
+            return this.medidasLados; 
+        }
+
+
+        public float getArea()
+        {
+            return this.AreaTriangulo;
+        }
+
+        public float getPerimetro()
+        {
+            return this.perimetro;
+        }
+
+        private void calculaPerimetro()
+        {
+            foreach (var p in this.getMedidasLados())
+            {
+                this.perimetro += p;
+            }
+        }
+
+        public String toString()
+        {
+            float lado1 = this.getMedidasLados().ElementAt(0);
+            float lado2 = this.getMedidasLados().ElementAt(1);
+            float lado3 = this.getMedidasLados().ElementAt(2);
+
+            return "Lado AB = " + lado1 + " | Lado BC = " + lado2 + " | Lado AC = " + lado3 + " | TIPO: "+ this.getTipoTriangulo() +" | Area = "+this.getArea();
+        }
+
+        public List<Vertice> GetVertices() { return this.vertices; }
+
+        private bool checkIfTriangule(Vertice v1, Vertice v2, Vertice v3)
+        {
+            this.getMedidasLados().Add(v1.euclidianDistance(v2));
+            this.getMedidasLados().Add(v2.euclidianDistance(v3));
+            this.getMedidasLados().Add(v3.euclidianDistance(v1));
+
+            if (getMedidasLados().ElementAt(0) + getMedidasLados().ElementAt(1) > getMedidasLados().ElementAt(2))
+            {
+                if (getMedidasLados().ElementAt(0) + getMedidasLados().ElementAt(2) > getMedidasLados().ElementAt(1))
+                {
+                    if (getMedidasLados().ElementAt(2) + getMedidasLados().ElementAt(1) > getMedidasLados().ElementAt(0))
+                    {
+                        return true;
+                    }
+                }
+            }
+            this.getMedidasLados().Clear();
+            return false;
+
+        }
+
+        public bool isEqualTriangulo(Triangulo t2)
+        {
+            if (this.getMedidasLados().ElementAt(0) == t2.getMedidasLados().ElementAt(0))
+            {
+                if (this.getMedidasLados().ElementAt(1) == t2.getMedidasLados().ElementAt(1))
+                {
+                    if (this.getMedidasLados().ElementAt(2) == t2.getMedidasLados().ElementAt(2))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public bool isSemelhanteTriangulo(Triangulo t2)
+        {
+            //Semelhança LLL
+            float proporcaoAB = this.getMedidasLados().ElementAt(0) / t2.getMedidasLados().ElementAt(0);
+            float proporcaoBC = this.getMedidasLados().ElementAt(1) / t2.getMedidasLados().ElementAt(1);
+            float proporcaoAC = this.getMedidasLados().ElementAt(2) / t2.getMedidasLados().ElementAt(2);
+
+            if (proporcaoAB == proporcaoBC) { 
+                if (proporcaoBC == proporcaoAC)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+    }
+}
